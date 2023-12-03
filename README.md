52deb65b37e1b8d2393450fd4896d6ddc805eea7
faridhwisanggeni <faridhwisanggeni@novis-MacBook-Pro.local>
2023-12-03 23:09:57 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # clean-architecture-sample
+
+This is sample for my student learning basic of SOLID design principle with clean code.
+
+
+Here's the DDL of the table product
+
+
+`-- DROP TABLE public.product;
+
+CREATE TABLE public.product (
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	"name" varchar(100) NULL,
+	price numeric NULL,
+	quantity int4 NULL,
+	CONSTRAINT product_pk PRIMARY KEY (id)
+);
+CREATE INDEX product_id_idx ON public.product USING btree (id, name);`
---
+++ infrastructure/product_repository_impl.go
@@ -0,0 +1,90 @@
+package infrastructure
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cleanarchitect/pos/domain"
+	"github.com/jackc/pgx/v4"
+)
+
+type ProductRepositoryImpl struct {
+	dbConn *pgx.Conn
+}
+
+func NewProductRepository(dbConn *pgx.Conn) *ProductRepositoryImpl {
+	return &ProductRepositoryImpl{
+		dbConn: dbConn,
+	}
+}
+
+func (r *ProductRepositoryImpl) GetProducts() ([]*domain.Product, error) {
+	sql := "SELECT id, name, price FROM product"
+
+	rows, err := r.dbConn.Query(context.Background(), sql)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching product: %w", err)
+	}
+	defer rows.Close()
+
+	var products []*domain.Product
+	for rows.Next() {
+		var p domain.Product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, fmt.Errorf("error scanning product: %w", err)
+		}
+		products = append(products, &p)
+	}
+
+	return products, nil
+}
+
+func (r *ProductRepositoryImpl) GetProductByID(id string) (*domain.Product, error) {
+	sql := "SELECT id, name, price FROM product WHERE id = $1"
+
+	row := r.dbConn.QueryRow(context.Background(), sql, id)
+
+	var p domain.Product
+	if err := row.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+		return nil, fmt.Errorf("error scanning product: %w", err)
+	}
+
+	return &p, nil
+}
+
+func (r *ProductRepositoryImpl) CreateProduct(product *domain.Product) (*domain.Product, error) {
+	sql := "INSERT INTO product(name, price) VALUES($1, $2) RETURNING id, name, price"
+
+	row := r.dbConn.QueryRow(context.Background(), sql, product.Name, product.Price)
+
+	var createdProduct domain.Product
+	if err := row.Scan(&createdProduct.ID, &createdProduct.Name, &createdProduct.Price); err != nil {
+		return nil, fmt.Errorf("error creating product: %w", err)
+	}
+
+	return &createdProduct, nil
+}
+
+func (r *ProductRepositoryImpl) UpdateProduct(id string, name string, price float64) (*domain.Product, error) {
+	sql := "UPDATE product SET name=$2, price=$3 WHERE id=$1 RETURNING id, name, price"
+
+	row := r.dbConn.QueryRow(context.Background(), sql, id, name, price)
+
+	var updatedProduct domain.Product
+	if err := row.Scan(&updatedProduct.ID, &updatedProduct.Name, &updatedProduct.Price); err != nil {
+		return nil, fmt.Errorf("error updating product: %w", err)
+	}
+
+	return &updatedProduct, nil
+}
+
+func (r *ProductRepositoryImpl) DeleteProduct(id string) error {
+	sql := "DELETE FROM product WHERE id = $1"
+
+	_, err := r.dbConn.Exec(context.Background(), sql, id)
+	if err != nil {
+		return fmt.Errorf("error deleting product: %w", err)
+	}
+
+	return nil
+}
